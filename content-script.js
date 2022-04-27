72d5c4857a365b78fbe5861180f730f0b2137b13
sydia1103 <69560065+sydia1103@users.noreply.github.com>
2022-04-27 17:25:21 -0500

    1.16.0.0 Fixed
    Fixed version of Good Old YouTube 1.16.0.0 from December 3, 2020.

--- content-script.js
+++ content-script.js
@@ -1,14 +1,5 @@
-function cleanup() {
-  const url = new URL(location.href);
-  const params = new URLSearchParams(url.search.replace('?', ''));
-  ['spf', 'pbj', 'spfreload', 'disable_polymer'].forEach(name => params.delete(name));
-  url.search = params.toString();
-  history.replaceState(history.state, document.title, url.toString());
-}
-
-function insertMenuItemAfter(el, id, text, url, icon) {
-  if (el) {
-    el.insertAdjacentHTML('afterEnd', `
+﻿function cleanup(){const url=new URL(location.href);const params=new URLSearchParams(url.search.replace('?',''));['spf','pbj','spfreload','disable_polymer'].forEach(name=>params.delete(name));url.search=params.toString();history.replaceState(history.state,document.title,url.toString());}
+function insertMenuItemAfter(el,id,text,url,icon){if(el){el.insertAdjacentHTML('afterEnd',`
       <li class="guide-channel guide-notification-item overflowable-list-item" id="${id}" role="menuitem">
         <a class="guide-item yt-uix-sessionlink yt-valign spf-link" href="${url}" title="${text}">
           <span class="yt-valign-container">
@@ -16,83 +7,15 @@ function insertMenuItemAfter(el, id, text, url, icon) {
               <span class="display-name no-count"><span>${text}</span></span>
           </span>
         </a>
-      </li>`);
-    return true;
-  }
-}
-
-function restoreHistory() {
-  const history = document.querySelector('#history-guide-item');
-  const donor = document.querySelector('.footer-history[href="/feed/history"]');
-  if (history || !donor) {
-    return;
-  }
-  const linkText = donor.textContent || 'History';
-  ['trending-guide-item', 'what_to_watch-guide-item']
-    .some(id => insertMenuItemAfter(document.querySelector(`#${id}`), 'history-guide-item', linkText, 'https://www.youtube.com/feed/history', 'guide-history-icon'));
-}
-
-function restoreMyChannel() {
-  const donor = document.querySelector('#creation-live-menu-item');
-  if (document.querySelector('#my-channel-guide-item') || !donor) {
-    return;
-  }
-  const url = donor.href.replace('studio.youtube.com', 'youtube.com').replace('/livestreaming', '');
-  if (url.indexOf('create_channel') >= 0) {
-    return;
-  }
-  const linkText = chrome.i18n.getMessage('my_channel_menu_title');
-  ['what_to_watch-guide-item', 'trending-guide-item']
-    .some(id => insertMenuItemAfter(document.querySelector(`#${id}`), 'my-channel-guide-item', linkText, url, 'guide-my-channel-icon'));
-}
-
-chrome.runtime.sendMessage({ type: 'getOptions' }, (options) => {
-  // Restore "show more" button
-  function restoreShowMoreButton() {
-    const actionPanel = document.getElementById('action-panel-details');
-    const showMore = document.querySelector('button[data-gen204="feature=watch-show-more-metadata"]');
-
-    if (!actionPanel || showMore) {
-      return;
-    }
-
-    actionPanel.insertAdjacentHTML('beforeend',
-      '<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander yt-uix-expander-head yt-uix-expander-collapsed-body yt-uix-gen204" type="button" onclick="return false;" data-gen204="feature=watch-show-more-metadata"><span class="yt-uix-button-content">Show more</span></button>' +
-      '<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander yt-uix-expander-head yt-uix-expander-body" type="button" onclick="return false;"><span class="yt-uix-button-content">Show less</span></button>'
-    );
-  }
-
-  // Force page reload when navigating between old/new interfaces or different page fixes
-  document.documentElement.addEventListener('click', e => {
-    let node = e.target;
-    const anchor = node.closest('a');
-    if (!anchor) {
-      return;
-    }
-
-    const isOldInterface = document.body.hasAttribute('data-spf-name');
-    const currentPageFixMethod = isOldInterface ? getPageFixMethod(location.href, options) : 'off';
-    const method = getPageFixMethod(anchor.href, options);
-    if (method === currentPageFixMethod) {
-      return;
-    }
-
-    const spLinkSelector = isOldInterface ? 'spf-link' : 'yt-simple-endpoint';
-    while (node && node.parentElement) {
-      node.classList.remove(spLinkSelector);
-      node = node.parentElement;
-    }
-  }, true);
-
-  const cleanupAndRestore = () => {
-    cleanup();
-    restoreShowMoreButton();
-    restoreHistory();
-    restoreMyChannel();    
-  }
-
-  window.addEventListener('spfdone', cleanupAndRestore);
-  window.addEventListener('DOMContentLoaded', cleanupAndRestore);
-});
-
-cleanup();
+      </li>`);return true;}}
+function restoreHistory(){const history=document.querySelector('#history-guide-item');const donor=document.querySelector('.footer-history[href="/feed/history"]');if(history||!donor){return;}
+const linkText=donor.textContent||'History';['trending-guide-item','what_to_watch-guide-item'].some(id=>insertMenuItemAfter(document.querySelector(`#${id}`),'history-guide-item',linkText,'https://www.youtube.com/feed/history','guide-history-icon'));}
+function restoreMyChannel(){const donor=document.querySelector('#creation-live-menu-item');if(document.querySelector('#my-channel-guide-item')||!donor){return;}
+const url=donor.href.replace('studio.youtube.com','youtube.com').replace('/livestreaming','');if(url.indexOf('create_channel')>=0){return;}
+const linkText=chrome.i18n.getMessage('my_channel_menu_title');['what_to_watch-guide-item','trending-guide-item'].some(id=>insertMenuItemAfter(document.querySelector(`#${id}`),'my-channel-guide-item',linkText,url,'guide-my-channel-icon'));}
+chrome.runtime.sendMessage({type:'getOptions'},(options)=>{function restoreShowMoreButton(){const actionPanel=document.getElementById('action-panel-details');const showMore=document.querySelector('button[data-gen204="feature=watch-show-more-metadata"]');if(!actionPanel||showMore){return;}
+actionPanel.insertAdjacentHTML('beforeend','<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander yt-uix-expander-head yt-uix-expander-collapsed-body yt-uix-gen204" type="button" onclick="return false;" data-gen204="feature=watch-show-more-metadata"><span class="yt-uix-button-content">Show more</span></button>'+'<button class="yt-uix-button yt-uix-button-size-default yt-uix-button-expander yt-uix-expander-head yt-uix-expander-body" type="button" onclick="return false;"><span class="yt-uix-button-content">Show less</span></button>');}
+document.documentElement.addEventListener('click',e=>{let node=e.target;const anchor=node.closest('a');if(!anchor){return;}
+const isOldInterface=document.body.hasAttribute('data-spf-name');const currentPageFixMethod=isOldInterface?getPageFixMethod(location.href,options):'off';const method=getPageFixMethod(anchor.href,options);if(method===currentPageFixMethod){return;}
+const spLinkSelector=isOldInterface?'spf-link':'yt-simple-endpoint';while(node&&node.parentElement){node.classList.remove(spLinkSelector);node=node.parentElement;}},true);const cleanupAndRestore=()=>{cleanup();restoreShowMoreButton();restoreHistory();restoreMyChannel();}
+window.addEventListener('spfdone',cleanupAndRestore);window.addEventListener('DOMContentLoaded',cleanupAndRestore);});cleanup();
--- options/default-options.js
+++ options/default-options.js
@@ -1,51 +1,5 @@
-const DEFAULT_OPTIONS = {
-  pageHome: 'headers',
-  pageFeed: 'headers',
-  pageVideo: 'headers',
-  pagePlaylist: 'headers',
-  pageChannel: 'headers',
-  pageGaming: 'headers',
-  pageResults: 'off',
-  pageOthers: 'headers',
-};
-
-const bg = chrome.extension.getBackgroundPage();
-if (!bg._options) { 
-  bg._options = Object.assign({}, DEFAULT_OPTIONS);
-}
-
-const _options = bg._options;
-
-function getOptions(main) {
-  chrome.storage.local.get(({ options }) => {
-    Object.assign(_options, options);
-    main(_options);
-  });
-};
-
-function setOption(key, value) {
-  if (value === DEFAULT_OPTIONS[key]) {
-    return resetOption(key);
-  }
-  chrome.storage.local.get(({ options }) => {
-    _options[key] = value;
-    chrome.storage.local.set({ 
-      options: Object.assign({}, options, { [key]: value }),
-    });
-  });
-}
-
-function resetOption(key) {
-  _options[key] = DEFAULT_OPTIONS[key];
-  chrome.storage.local.get(({ options = {} }) => {
-    delete options[key];
-    chrome.storage.local.set({
-      options: Object.assign({}, options),
-    });
-  });
-}
-
-function resetOptions() {
-  Object.assign(_options, DEFAULT_OPTIONS);
-  chrome.storage.local.set({ options: {} });
-}
+﻿const DEFAULT_OPTIONS={pageHome:'headers',pageFeed:'headers',pageVideo:'headers',pagePlaylist:'headers',pageChannel:'headers',pageGaming:'headers',pageResults:'off',pageOthers:'headers',};const bg=chrome.extension.getBackgroundPage();if(!bg._options){bg._options=Object.assign({},DEFAULT_OPTIONS);}
+const _options=bg._options;function getOptions(main){chrome.storage.local.get(({options})=>{Object.assign(_options,options);main(_options);});};function setOption(key,value){if(value===DEFAULT_OPTIONS[key]){return resetOption(key);}
+chrome.storage.local.get(({options})=>{_options[key]=value;chrome.storage.local.set({options:Object.assign({},options,{[key]:value}),});});}
+function resetOption(key){_options[key]=DEFAULT_OPTIONS[key];chrome.storage.local.get(({options={}})=>{delete options[key];chrome.storage.local.set({options:Object.assign({},options),});});}
+function resetOptions(){Object.assign(_options,DEFAULT_OPTIONS);chrome.storage.local.set({options:{}});}
