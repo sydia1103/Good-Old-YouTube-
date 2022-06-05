f17a9385c0ffb686465876fc1e9744710405057e
sydia1103 <69560065+sydia1103@users.noreply.github.com>
2022-06-05 11:50:46 -0500

    1.16.0 Fixed
    Fixed version of Good Old YouTube from December 3, 2020.

--- options/options.js
+++ options/options.js
@@ -1,54 +1,20 @@
-const OPTIONS = [
-  'pageHome', 'pageFeed', 'pageVideo', 'pagePlaylist', 'pageChannel', 'pageGaming', 'pageResults', 'pageOthers'
-];
+﻿const OPTIONS = ['pageHome', 'pageFeed', 'pageVideo', 'pagePlaylist', 'pageChannel', 'pageGaming', 'pageResults', 'pageOthers'];
 
-function showCurrentOptions(options) {
-  OPTIONS.forEach(id => {
-    const input = document.getElementById(id);
-    if (input.type === 'checkbox') {
-      input[options[id] ? 'setAttribute' : 'removeAttribute']('checked', true);
-    } else {
-      input.value = options[id];
-    }
-  });
-}
-
+function showCurrentOptions(options) { OPTIONS.forEach(id => { const input = document.getElementById(id); if (input.type === 'checkbox') { input[options[id] ? 'setAttribute' : 'removeAttribute']('checked', true); } else { input.value = options[id]; } }); }
 getOptions((options) => {
-  function generatePageFixOptions() {
-    const html = Object.keys(PAGES).map((key) => {
-      const { name, methods } = PAGES[key];
-      const optNames = [...new Set(methods.concat(['headers', 'off']))];
-      const opts = optNames.map(n => {
-        return `<option value="${n}" ${options[key] === n ? 'selected' : ''}>
+    function generatePageFixOptions() { const html = Object.keys(PAGES).map((key) => { const { name, methods } = PAGES[key]; const optNames = [...new Set(methods.concat(['headers', 'off']))]; const opts = optNames.map(n => { return `<option value="${n}" ${options[key] === n ? 'selected' : ''}>
           ${n} ${(n === 'off' || methods.includes(n)) ? '' : '(broken)'}
-        </option>`
-      }).join('');
-      return `
+        </option>` }).join(''); return `
       <div>
         <label for="${key}">${name}</label>
         <select id="${key}">${opts}</select>
       </div>
-    `;
-    }).join('');
-
-    document.getElementById('pageFixes').innerHTML = html;
-  }
-
-  generatePageFixOptions();
-
-  showCurrentOptions(options);
-
-  OPTIONS.forEach(id => {
-    const input = document.getElementById(id);
-    input.addEventListener('change', () => setOption(id,
-      input[input.type === 'checkbox' ? 'checked' : 'value']
-    ));
-  });
-
-  document.body.addEventListener('click', (ev) => {
-    if (ev.target.id === 'resetAll') {
-      resetOptions();
-      showCurrentOptions(options);
-    }
-  });
+    `; }).join('');
+        document.getElementById('pageFixes').innerHTML = html; }
+    generatePageFixOptions();
+    showCurrentOptions(options);
+    OPTIONS.forEach(id => { const input = document.getElementById(id);
+        input.addEventListener('change', () => setOption(id, input[input.type === 'checkbox' ? 'checked' : 'value'])); });
+    document.body.addEventListener('click', (ev) => { if (ev.target.id === 'resetAll') { resetOptions();
+            showCurrentOptions(options); } });
 });
